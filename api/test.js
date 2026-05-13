6ae05e33ac7235c697b7ffd24eb11e9ee4fa2b40
narayanglokhande2007-sudo <narayanglokhande2007@gmail.com>
2026-05-13 07:26:22 +0530

    test whatsapp verify

--- api/test.js
+++ api/test.js
@@ -1,3 +1,16 @@
 export default function handler(req, res) {
-  res.status(200).send('Test OK from new file!');
+  const VERIFY_TOKEN = process.env.WHATSAPP_VERIFY_TOKEN;
+
+  if (req.method === 'GET') {
+    const mode = req.query['hub.mode'];
+    const token = req.query['hub.verify_token'];
+    const challenge = req.query['hub.challenge'];
+
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      return res.status(200).send(challenge);
+    }
+    return res.status(200).send('WhatsApp route is alive!');
+  }
+
+  res.status(200).send('POST ok');
 }
